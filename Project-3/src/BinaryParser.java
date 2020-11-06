32f497e0aca2dc8d97e92810052d81e22e667956
candy <candy@vt.edu>
2020-11-06 13:27:22 -0500

    Skeleton and selection sort

---
+++ Project-3/src/BinaryParser.java
@@ -0,0 +1,10 @@
+
+public class BinaryParser {
+	
+	private String fileName;
+	private byte[] input;
+	
+	public BinaryParser(String binaryFile) {
+		//Use buffer stream or some other input command
+	}
+}
---
+++ Project-3/src/SelectionSort.java
@@ -0,0 +1,17 @@
+/**
+ * This class will sort the heap of records by selection.
+ * 
+ * @author justin, Andy
+ *
+ */
+public class SelectionSort {
+	private Heap minHeap;
+	private byte[] inputBuffer;
+	private byte[] outputBuffer;
+	
+	public SelectionSort(Heap heap) {
+		minHeap = heap;
+	}
+	
+	
+}
