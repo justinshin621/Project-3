bc4e631a17046759df45ca9c4f6a78d2417bd5b4
Justin <justinshin@vt.edu>
2020-11-07 21:33:06 -0500

    Updating Heaptest for Record

--- Project-3/src/HeapTest.java
+++ Project-3/src/HeapTest.java
@@ -16,15 +16,15 @@ public class HeapTest extends TestCase {
         Heap heap = new Heap(8);
         assertEquals(0, heap.getSize());
         
-        Record rec1 = new Record(1, 1.5f);
-        Record rec2 = new Record(2, 1.0f);
-        Record rec3 = new Record(3, 1.8f);
-        Record rec4 = new Record(4, 4.0f);
-        Record rec5 = new Record(5, 0.4f);
-        Record rec6 = new Record(6, 1.7f);
-        Record rec7 = new Record(6, 0.5f);
-        Record rec8 = new Record(6, 1.6f);
-
+        
+        Record rec1 = new Record(new byte[] {0x17, 0x3B, (byte)0xF5, 0x44, 0x7F, 0x47, 0x69, 0x28});
+        Record rec2 = new Record(new byte[] {0x11, 0x4B, (byte)0x55, 0x14, 0x7F, 0x42, 0x49, 0x58});
+        Record rec3 = new Record(new byte[] {0x17, 0x3B, (byte)0xF2, 0x14, 0x7F, 0x37, 0x69, 0x58});
+        Record rec4 = new Record(new byte[] {0x27, 0x3B, (byte)0xF5, 0x14, 0x7F, 0x43, 0x69, 0x28});
+        Record rec5 = new Record(new byte[] {0x13, 0x4B, (byte)0xF5, 0x64, 0x7F, 0x42, 0x19, 0x28});
+        Record rec6 = new Record(new byte[] {0x27, 0x33, (byte)0xF5, 0x74, 0x2F, 0x31, 0x64, 0x28});
+        Record rec7 = new Record(new byte[] {0x15, 0x1B, (byte)0xF5, 0x44, 0x2F, 0x27, 0x19, 0x38});
+        Record rec8 = new Record(new byte[] {0x12, 0x3B, (byte)0xF5, 0x42, 0x2F, 0x27, 0x29, 0x28});
         
         assertNull(heap.removeMin());
         assertEquals(-1, heap.parent(-1));
