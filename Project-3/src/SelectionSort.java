22dc5a4d9e2deb4433c2638b95557e114103caba
candy <candy@vt.edu>
2020-11-08 14:01:18 -0500

    added unfinished code for 16> blocks

--- Project-3/src/SelectionSort.java
+++ Project-3/src/SelectionSort.java
@@ -11,76 +11,69 @@ import java.io.PrintWriter;
  *
  */
 public class SelectionSort {
-    
-    private static Heap minHeap = new Heap(16384);
-    
-    public static void read(String binaryFile, String outputFile) {
-        try {
-            RandomAccessFile file = new RandomAccessFile(binaryFile, "r");
-            
-            sort(file, outputFile);
-            file.close();
-        }
-        catch (IOException e) {
-            System.out.println("File not found");
-        }
-    }
-    
-    /**
-     * This function is used to sort the input file using Selection Sort and 
-     * multi-way merge (if needed) and output it to outputFile
-     * 
-     * @param raf Our input binary file as a RandomAccessFile
-     * @param outputFile Our output file 
-     * @throws IOException An IO exception if PrintWriter not valid
-     */
-    public static void sort(RandomAccessFile raf, String outputFile) throws IOException {
-        
-        int numOfBlocks = (int)(raf.length()/8192); //  Receiving the # of blocks of the input file
 
-        
-        // Handles the case if the data file is less than or equal to 16 blocks of data
-        if (numOfBlocks <= 16) {
-            
-            // We will be first storing all of the data into a byte array
-            byte[] inputData = new byte[(int)raf.length()]; 
-            raf.read(inputData);
-            
-            // We then grab respective range of bytes from input array and add new records into the heap
-            for (int i = 0; i < inputData.length; i+=8) {
-                Record temp = new Record(Arrays.copyOfRange(inputData, i, i+8));
-                minHeap.insert(temp);
-            }
-            
-            System.out.println(minHeap.getSize());
-        }
+	private static Heap minHeap = new Heap(16384);
+	public static final int BLOCK_SIZE = 8192;
 
-        
-        // This 
-        if (numOfBlocks <= 16) {
-            
-        }
-        
-                
-        
-        
-            
+	public static void read(String binaryFile, String outputFile) {
+		try {
+			RandomAccessFile file = new RandomAccessFile(binaryFile, "r");
 
-          
-        
+			sort(file, outputFile);
+			file.close();
+		} catch (IOException e) {
+			System.out.println("File not found");
+		}
+	}
 
-                
-        PrintWriter pw = new PrintWriter(outputFile);
-        
-        pw.close();
+	/**
+	 * This function is used to sort the input file using Selection Sort and
+	 * multi-way merge (if needed) and output it to outputFile
+	 * 
+	 * @param raf        Our input binary file as a RandomAccessFile
+	 * @param outputFile Our output file
+	 * @throws IOException An IO exception if PrintWriter not valid
+	 */
+	public static void sort(RandomAccessFile raf, String outputFile) throws IOException {
 
-    }
-    
-    public void updateInput() {
-        
-    }
-    
-    public void updateOutput() {
-        
-    }
+		int numOfBlocks = (int) (raf.length() / BLOCK_SIZE); // Receiving the # of blocks of the input file
+
+		// Handles the case if the data file is less than or equal to 16 blocks of data
+		if (numOfBlocks <= 16) {
+
+			// We will be first storing all of the data into a byte array
+			byte[] inputData = new byte[(int) raf.length()];
+			raf.read(inputData);
+
+			// We then grab respective range of bytes from input array and add new records
+			// into the heap
+			for (int i = 0; i < inputData.length; i += 8) {
+				Record temp = new Record(Arrays.copyOfRange(inputData, i, i + 8));
+				minHeap.insert(temp);
+			}
+
+			System.out.println(minHeap.getSize());
+
+			return;
+		}
+
+		// Edge case that deals with when the number of blocks
+		// in the binary file is greater than 16
+		for (int i = 0; i < numOfBlocks; i++) {
+			byte[] inputData = new byte[BLOCK_SIZE];
+			if (raf.read(inputData, 0, BLOCK_SIZE) != -1) {
+				// Place
+				for (int j = 0; j < inputData.length; j += 8) {
+					Record temp = new Record(Arrays.copyOfRange(inputData, j, j + 8));
+					minHeap.insert(temp);
+				}
+			}
+		}
+
+		PrintWriter pw = new PrintWriter(outputFile);
+
+		pw.close();
+
+	}
+
 }
